cadf221c515cda0128ea99bea63117b3481a6221
Your Name <you@example.com>
2023-10-18 01:21:01 +0900

    2주차 공부중

--- js-week2/01.js
+++ js-week2/01.js
@@ -8,21 +8,21 @@
 
 // }
 
-// var add = function () {
+var add = function () {
 
-// }
+}
 
-// var add = () => {
-//     return 1;
-// }
+var add = () => {
+    return 1;
+}
 
-// var add = () => 1;
+var add = () => 1;
 
 
 // 삼항 연산자
-//condition ? true인경우 : false인경우
+condition ? true인경우 : false인경우
 
-// console.log(true ? '참' : '거짓');
-// console.log(false ? '참' : '거짓');
-// console.log(1 === 1 ? '참' : '거짓');
-// console.log(1 !== 1 ? '참' : '거짓');
+console.log(true ? '참' : '거짓');
+console.log(false ? '참' : '거짓');
+console.log(1 === 1 ? '참' : '거짓');
+console.log(1 !== 1 ? '참' : '거짓');
--- js-week2/02.js
+++ js-week2/02.js
@@ -4,48 +4,40 @@
 // 배열이나, 객체의 속성
 
 // (1) 배열의 경우
-// let [value1, value2] = [1, "new"];
-// console.log("1", value1)
-// console.log("2", value2)
-// let arr = ["value1", "value2", "value3", "value4" ];
-// let [a, b, c, d=4(초깃값)] = arr;
+let [value1, value2] = [1, "new"];
+console.log("1", value1)
+console.log("2", value2)
+let arr = ["value1", "value2", "value3", "value4"];
+let [a, b, c, d = 4(초깃값)] = arr;
 
-// console.log(a)
-// console.log(b)
-// console.log(c)
-// console.log(d)
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
 
 // (2) 객체인 경우
-// let user = {
-//     name: "nbc",
-//     age: 30,
-
-// };
-
-// 구조분해할당
-// let { name, age } = {
-//     name: "nbc",
-//     age: 30,
-// }
-
-// console.log("name =>", name); // string
-// console.log("age =>", age);
-
-// 새로운 이름으로 할당
 let user = {
     name: "nbc",
     age: 30,
-    birthday: "yesterday"
+
 };
 
-// let {
-//     name: newName,
-//     age: newAge
-// } = user;
-// console.log("newName =>", newName);
-// console.log("newAge =>", newAge);
+// 구조분해할당
+let { name, age } = {
+    name: "nbc",
+    age: 30,
+}
 
-let {name, age, birthday = "today"} = user;
-console.log(name)
-console.log(age)
-console.log(birthday)
+console.log("name =>", name); // string
+console.log("age =>", age);
+
+// 새로운 이름으로 할당
+let { name: newName, age: newAge } = user;
+console.log(name, age) // ReferenceError: name is not defined
+console.log(newName, newAge) //nbc 30
+
+let { name, age, birthDay } = user;
+console.log(birthDay) // undefined
+
+let { name, age, birthDay = "today" } = user;
+console.log(birthDay) // today
